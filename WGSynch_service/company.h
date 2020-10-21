9a8aba4907334b1ea58b2bdb80f1a11f1d2e2dea
Николай Полтавский <n.poltavskiy@5-tv.ru>
2020-10-21 19:02:11 +0300

    Go

--- WGSynch_service/company.h
+++ WGSynch_service/company.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <vector>
+#include "user.h"
+
 class company {
+private:
+	std::vector<user> company_users_;
+public:
+	explicit company();
+
+	void Add_Users(user user);
+	std::vector<user> Get_Users();
 };
 
