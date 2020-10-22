d8c67ef2466f37eb5a99a75001751a636360b58b
Николай Полтавский <n.poltavskiy@5-tv.ru>
2020-10-22 18:43:27 +0300

    add property main

--- WGSynch_service/main.cpp
+++ WGSynch_service/main.cpp
@@ -2,15 +2,27 @@
 #include <mysql.h>
 #include "FS.h"
 #include <windows.h>
+using namespace std::string_literals;
 
 int main(int argc, char* argv[]) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	FS fs_main;
-	fs_main.init_settings();
-	std::cout << std::endl;
-	fs_main.init_all_users_base();
+	
+	if (argc > 1 && argv[1][0] == '/') {
+		if (argv[1][1] == 'p') {
+			FS fs_main(argv[2]);
+			fs_main.init_settings();
+			std::cout << std::endl;
+			fs_main.init_all_users_base();
+		}
+	} else {
+		FS fs_main;
+		fs_main.init_settings();
+		std::cout << std::endl;
+		fs_main.init_all_users_base();
+	}
 
+	std::cout << "TEST SERVICE - OK"s << std::endl;
 	return 0;
 }
