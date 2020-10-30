42f77d64e69c875bbafa0f59316137b9d84d68dc
Poltavskiy Nick <unluck.bastard@gmail.com>
2020-10-30 12:53:50 +0300

    this

--- employee.php
+++ employee.php
@@ -1,8 +1,9 @@
 <?php
 class Credential{
     private $dep_query_;
+
     public Credential($dep_query){
-        $dep_query_ = $dep_query;
+        this->$dep_query_ = $dep_query;
     }
 
     public function sql_query(){
@@ -13,7 +14,7 @@ class Credential{
             die("Connection failed: " . $conn->connect_error);
         }
         $conn->set_charset("utf8");
-        $query = "SELECT * FROM root WHERE status LIKE '1' and dep like '$dep_query_'";
+        $query = "SELECT * FROM root WHERE status LIKE '1' and dep like 'this->$dep_query_'";
         $result = $conn->query($query);
 
         if($result->num_rows>0){
