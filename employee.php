344690a060b02ae05ed0471997b68f6166ca68ea
Poltavskiy Nick <unluck.bastard@gmail.com>
2020-10-30 12:23:51 +0300

    all

--- employee.php
+++ employee.php
@@ -6,13 +6,13 @@
         die("Connection failed: " . $conn->connect_error);
     }
     $conn->set_charset("utf8");
-    $query = "SELECT account FROM root WHERE status LIKE '1' and dep like 'Служба компьютерных технологий%'";
+    $query = "SELECT * FROM root WHERE status LIKE '1' and dep like 'Служба компьютерных технологий%'";
     $result = $conn->query($query);
 
     if($result->num_rows>0){
         while($row = $result->fetch_assoc()){
             
-            echo '<div>'.$row["account"].'</div>';
+            echo '<div>'.$row["name"].'</div>';
         }
     }
 
