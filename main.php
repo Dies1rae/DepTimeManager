e207261a8a0bafbeb83779e4db04d214642bb2dc
Konstantin <kostik06@gmail.com>
2020-11-03 14:09:24 +0300

    modal window

--- main.php
+++ main.php
@@ -16,6 +16,7 @@ if(!session_id() || session_status() !== PHP_SESSION_ACTIVE) {
     <link rel="stylesheet" type="text/css" href="style/regular.css">
     <link rel="stylesheet" type="text/css" href="style/fontawesome.css">
     <link rel="stylesheet" type="text/css" href="style/calendar.css">
+    <link rel="stylesheet" type="text/css" href="style/popupwindow.css">
 </head>
 <body>
     <div id="container" class="wrapper">
@@ -109,5 +110,51 @@ if(!session_id() || session_status() !== PHP_SESSION_ACTIVE) {
 		    ?>
         </aside>
     </div>
+
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Modal Header</h2>
+        </div>
+        <div class="modal-body">
+            <p>Some text in the Modal Body</p>
+            <p>Some other text...</p>
+        </div>
+        <div class="modal-footer">
+            <h3>Modal Footer</h3>
+        </div>
+        </div>
+    </div>
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementByClassName("b_main_time");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+        modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        }
+</script>
 </body>
 </html>
