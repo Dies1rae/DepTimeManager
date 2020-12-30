acc47e6c0a481074818e21cb1bf00fdc17807e85
Konstantin <kostik06@gmail.com>
2020-12-30 14:04:07 +0300

    adasd

--- main.php
+++ main.php
@@ -475,7 +475,7 @@ if(!isset($_SESSION['dep_key'])) {
             <fieldset id="gButtons">
                 <table border="5px" width="100%">
                     <tr>
-                        <td colspan="2"><input type="submit" id="testRuid" value="Сохранить"></td>
+                        <td class="btnR"><input type="submit" id="testRuid" value="Сохранить"><input type="button" id="cancelBtn" value="Отменить"></td>
                     </tr>
                 </table>
             </fieldset>
@@ -489,11 +489,15 @@ if(!isset($_SESSION['dep_key'])) {
 
         // Get the <span> element that closes the modal
         let span = document.getElementsByClassName("close")[0];
-
+        let cancelButton = document.getElementById("cancelBtn");
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
         modal.style.display = "none";
         }
+        cancelButton.onclick = function(){
+            modal.style.display = "none";
+        }
+
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
